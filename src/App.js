fb7c4b96b6cfe789280652b3879fe7c429dc6adc
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-06-30 10:54:17 +0200

    feat: enable URI rewriting (#40)

--- src/App.js
+++ src/App.js
@@ -2,85 +2,102 @@ import "./App.css";
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 const defaultEndpoint = "http://rdf.insee.fr/sparql";
+const defaultPrefix = "https://rdf.insee.fr/sparql/describe?uri=";
 
-function Editor({ endpoint, queries }) {
-  const [yasgui, setYasgui] = useState()
-  const [counter, setCounter] = useState(0)
+function Editor({ endpoint, queries, prefix }) {
+  const [yasgui, setYasgui] = useState();
+  const [counter, setCounter] = useState(0);
   const ref = useRef(null);
 
-  const [inserted, setInserted] = useState(false)
+  const [inserted, setInserted] = useState(false);
 
   useLayoutEffect(() => {
-    if(counter > 100 || inserted){
+    if (counter > 100 || inserted) {
       return;
     }
-    if(document.querySelector(".yasqe")){
-      document.querySelector(".yasqe").appendChild(ref.current)
-      setInserted(true)
+    if (document.querySelector(".yasqe")) {
+      document.querySelector(".yasqe").appendChild(ref.current);
+      setInserted(true);
     }
-    setCounter(counter + 1)
-  }, [counter, inserted])
+    setCounter(counter + 1);
+  }, [counter, inserted]);
 
   const click = (query) => {
     fetch(query.path)
-      .then(response => response.text())
-      .then(body => {
+      .then((response) => response.text())
+      .then((body) => {
         const tab = yasgui.getTab();
         tab.setQuery(body);
-      })  
-  }
+      });
+  };
 
   useLayoutEffect(() => {
     localStorage.removeItem("yagui__config");
-    // eslint-disable-next-line no-undef
-    setYasgui(new Yasgui(document.getElementById("editor"), {
-      requestConfig: {
-        endpoint
-      }
-    }))
+    setYasgui(
+      // eslint-disable-next-line no-undef
+      new Yasgui(document.getElementById("editor"), {
+        requestConfig: {
+          endpoint,
+        },
+      })
+    );
   }, [endpoint]);
 
   return (
     <>
       <div className="queries-block" ref={ref}>
-        {
-          queries.map((query, i) => <button key={i} onClick={() => click(query)}> {query.label}  </button>)
-        }
+        {queries.map((query, i) => (
+          <button key={i} onClick={() => click(query)}>
+            {" "}
+            {query.label}{" "}
+          </button>
+        ))}
       </div>
-      <div id="editor"></div>
+      <div
+        id="editor"
+        onClick={(e) => {
+          if (
+            endpoint !== defaultEndpoint &&
+            e.target.href &&
+            e.target.href.indexOf("http://id.insee.fr/") === 0 &&
+            e.target.href.indexOf(prefix) !== 0
+          ) {
+            e.target.href = prefix + encodeURIComponent(e.target.href);
+          }
+        }}
+      ></div>
     </>
   );
 }
 
 function App() {
   const [queries, setQueries] = useState([]);
+  const [prefix, setPrefix] = useState();
   const [endpoint, setEndpoint] = useState();
-  
+
   useEffect(() => {
     fetch("/queries/queries.json")
-      .then(response => response.json())
-      .then(body => {
-        setQueries(body)
-      })
+      .then((response) => response.json())
+      .then((body) => {
+        setQueries(body);
+      });
   }, []);
 
   //
   useEffect(() => {
     fetch("/configuration.json")
-      .then(response => response.json())
-      .then(configuration => {
-        setEndpoint(configuration.sparql_endpoint ?? defaultEndpoint)
+      .then((response) => response.json())
+      .then((configuration) => {
+        setEndpoint(configuration.sparql_endpoint ?? defaultEndpoint);
+        setPrefix(configuration.prefix ?? defaultPrefix)
       })
       .catch(() => {
-        setEndpoint(defaultEndpoint)
-      })
+        setEndpoint(defaultEndpoint);
+        setPrefix(defaultPrefix);
+      });
   }, []);
 
-  return (
-    <div className="App">
-      {endpoint && <Editor endpoint={endpoint} queries={queries}/> }
-    </div>
-  );
+  return <div className="App">{endpoint && <Editor endpoint={endpoint} queries={queries} prefix={prefix} />}</div>;
 }
 
 export default App;
