a78ab34d0b425a462d60ec3325dc0a5c9b850ec5
Emmanuel <demey.emmanuel@gmail.com>
2024-07-27 14:30:08 +0100

    feat: ini vitest

---
+++ src/App.test.jsx
@@ -0,0 +1,23 @@
+import { render } from "@testing-library/react";
+import App from "./App";
+import { test, vi } from "vitest";
+
+vi.stubGlobal("Yasgui", function Yasgui(e) {
+    const div = document.createElement("div");
+    div.classList.add("yasqe");
+    e.appendChild(div);
+});
+vi.stubGlobal("fetch", function fetch(url) {
+    return Promise.resolve({
+        json() {
+            if (url.includes("/configuration.json")) {
+                return Promise.resolve({});
+            }
+            return Promise.resolve([]);
+        }
+    });
+});
+
+test("renders the App component", () => {
+    render(<App />);
+});
