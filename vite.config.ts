e476db28e8ca10b4e40b967511f1bdb530b5b22f
Pierre Vasseur <pierre.vasseur@insee.fr>
2024-10-21 16:28:56 +0200

    reshape some files

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
 import { configDefaults } from "vitest/config";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
     base: "/",
     plugins: [react()],
     test: {
@@ -21,4 +20,4 @@ export default defineConfig({
     server: {
         port: 3000
     }
-});
+};
