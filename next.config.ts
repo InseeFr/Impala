932dc2b1b64c686a193c0300fd6db2567a6cf665
Emmanuel <demey.emmanuel@gmail.com>
2025-07-21 15:59:00 +0100

    fix: review

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
+import { name, version } from "./package.json";
 
 const nextConfig: NextConfig = {
+    env: {
+        NEXT_PUBLIC_NAME: name,
+        NEXT_PUBLIC_VERSION: version
+    },
     /* config options here */
     async rewrites() {
         return [
