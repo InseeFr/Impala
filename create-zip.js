a7d38e58ae3e01baae719edccb8a710cbb6d427d
Emmanuel Demey <demey.emmanuel@gmail.com>
2026-06-02 11:53:57 +0100

    fix: update dependencies

--- create-zip.js
+++ create-zip.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
 
-import archiver from 'archiver';
-import { createWriteStream } from 'fs';
-import { resolve } from 'path';
+import { ZipArchive } from "archiver";
+import { createWriteStream } from "fs";
+import { resolve } from "path";
 
-const output = createWriteStream(resolve('build', 'build.zip'));
-const archive = archiver('zip', {
-  zlib: { level: 9 }
+const output = createWriteStream(resolve("build", "build.zip"));
+const archive = new ZipArchive({
+    zlib: { level: 9 }
 });
 
-output.on('close', () => {
-  console.log(`Created build.zip (${archive.pointer()} bytes)`);
+output.on("close", () => {
+    console.log(`Created build.zip (${archive.pointer()} bytes)`);
 });
 
-archive.on('error', (err) => {
-  throw err;
+archive.on("error", err => {
+    throw err;
 });
 
 archive.pipe(output);
-archive.glob('**/*', { cwd: 'build', ignore: ['build.zip'] });
+archive.glob("**/*", { cwd: "build", ignore: ["build.zip"] });
 archive.finalize();
